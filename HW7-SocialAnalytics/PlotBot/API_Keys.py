1775c67acc492ad7fdfc3dc2d7d865b0ed91c401
Sumedh Vedanthi <sumedhgv@gmail.com>
2018-11-23 13:40:28 -0800

    Create API_Keys.py

---
+++ HW7-SocialAnalytics/PlotBot/API_Keys.py
@@ -0,0 +1,5 @@
+# Replace with your API keys
+consumer_key = "jCuMds8hkjry8JV8JDEuDVH9o"
+consumer_secret = "psgKB7nb05kZqoD2ZFPrG78OqbObHySWUEhcLFcZ03qVMlsCwp"
+access_token = "814999527451148288-PVho6BBmmcQbSVKOHBt3E5jbPJM6Krl"
+access_token_secret = "a30jMaE70P2kefPFOzrfGTlA06okUcifkjJB9g2JWq4Ih"
